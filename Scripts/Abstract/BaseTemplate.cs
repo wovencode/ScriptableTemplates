5e926d4c8760f2fa7e77dbd4ed3bfc44be243abd
Fhiz <support@wovencode.net>
2020-01-08 08:46:40 +0100

    Added RarityTemplate
    Added RarityTemplate

---
+++ Scripts/Abstract/BaseTemplate.cs
@@ -0,0 +1,42 @@
+﻿// =======================================================================================
+// BaseTemplate
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+using System;
+using UnityEngine;
+using wovencode;
+
+namespace wovencode {
+	
+	// ===================================================================================
+	// BaseTemplate
+	// ===================================================================================
+	public abstract partial class BaseTemplate : ScriptableTemplate
+	{
+		
+		public Sprite 			smallIcon;
+		public Sprite			backgroundIcon;
+		public RarityTemplate 	rarity;
+		
+		[TextArea(5,5)]
+		public string 			description;				
+		
+		// -------------------------------------------------------------------------------
+        // OnValidate
+        // if the title is empty, we simple copy the object name into the title
+        // note that we cannot cache the folderName here, because it would the same for all objects of this type
+        // -------------------------------------------------------------------------------
+		public override void OnValidate()
+		{
+			base.OnValidate();
+		}
+		
+		// -------------------------------------------------------------------------------
+		
+	}
+
+}
+
+// =======================================================================================
---
+++ Scripts/Example/RarityTemplate.cs
@@ -0,0 +1,82 @@
+﻿// =======================================================================================
+// RarityTemplate
+// by Weaver (Fhiz)
+// MIT licensed
+// =======================================================================================
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using wovencode;
+
+namespace wovencode {
+	
+	// ===================================================================================
+	// BaseTemplate
+	// ===================================================================================
+	[CreateAssetMenu(fileName = "New RarityTemplate", menuName = "Templates/New RarityTemplate", order = 999)]
+	public partial class RarityTemplate : ScriptableTemplate
+	{
+		
+		public Color			color;
+		public Sprite			borderImage;
+		
+		// -------------------------------------------------------------------------------
+		
+		public static string _folderName = "";	
+		
+		static Dictionary<int, RarityTemplate> _data;
+		
+		// -------------------------------------------------------------------------------
+        // data
+        // -------------------------------------------------------------------------------
+		public static Dictionary<int, RarityTemplate> data
+		{
+			get {
+				RarityTemplate.BuildCache();
+				return _data;
+			}
+		}
+		
+		// -------------------------------------------------------------------------------
+        // BuildCache
+        // -------------------------------------------------------------------------------
+		public static void BuildCache()
+		{
+			if (_data != null) return;
+
+			List<RarityTemplate> templates = Resources.LoadAll<RarityTemplate>(RarityTemplate._folderName).ToList();
+					
+			if (templates.HasDuplicates())
+				Debug.LogWarning("[Warning] Skipped loading due to duplicate(s) in Resources subfolder: " + RarityTemplate._folderName);
+			else
+				_data = templates.ToDictionary(x => x.name.GetDeterministicHashCode(), x => x);
+			
+		}
+		
+		// -------------------------------------------------------------------------------
+        // OnEnable
+        // -------------------------------------------------------------------------------
+		public void OnEnable()
+		{
+			if (_folderName != folderName)
+				_folderName = folderName;
+		}
+		
+		// -------------------------------------------------------------------------------
+        // OnValidate
+        // -------------------------------------------------------------------------------
+		public override void OnValidate()
+		{
+			base.OnValidate();
+		}
+		
+		// -------------------------------------------------------------------------------
+		
+	}
+
+}
+
+// =======================================================================================

b30d9fb69892c2928c3bbbfab3d9f35a076bf6d9
Fhiz <support@wovencode.net>
2020-01-23 18:12:16 +0100

    Update PreloaderComponent.cs

--- Scripts/Components [Optional]/PreloaderComponent.cs
+++ Scripts/Components [Optional]/PreloaderComponent.cs
@@ -12,39 +12,47 @@ using UnityEngine;
 using System.Reflection;
 using System;
 
-public class PreloaderComponent : MonoBehaviour
+namespace Wovencode
 {
 	
-	public ScriptableTemplate[] preloadTemplates;
-	
-	public bool preloadOnAwake;
-	
-	// -----------------------------------------------------------------------------------
-	// Awake
-	// -----------------------------------------------------------------------------------
-    void Awake()
-    {
-    	if (preloadOnAwake)
-    		PreloadTemplates();
-    }
-    
-	// -----------------------------------------------------------------------------------
-	// PreloadTemplates
-	// -----------------------------------------------------------------------------------
-	public void PreloadTemplates()
+	// ===================================================================================
+	// PreloaderComponent
+	// ===================================================================================
+	public class PreloaderComponent : MonoBehaviour
 	{
 		
-		foreach (ScriptableTemplate template in preloadTemplates)
+		[Header("Add one representative template of each type in Resources")]
+		public ScriptableTemplate[] preloadTemplates;
+		public bool preloadOnAwake;
+	
+		// -----------------------------------------------------------------------------------
+		// Awake
+		// -----------------------------------------------------------------------------------
+		void Awake()
 		{
-			Type t = template.GetType();
-			MethodInfo m = t.GetMethod("BuildCache");
-			m.Invoke(this, new object[]{true});
-
-
-		}	
+			if (preloadOnAwake)
+				PreloadTemplates();
+		}
+	
+		// -----------------------------------------------------------------------------------
+		// PreloadTemplates
+		// -----------------------------------------------------------------------------------
+		public void PreloadTemplates()
+		{
 		
+			foreach (ScriptableTemplate template in preloadTemplates)
+			{
+				// workaround that calls "BuildCache"
+				// as we cannot override static methods
+				Type t = template.GetType();
+				MethodInfo m = t.GetMethod("BuildCache");
+				m.Invoke(this, new object[]{true});
+			}	
+		
+		}
+	
+		// -----------------------------------------------------------------------------------
+	
 	}
-	
-	// -----------------------------------------------------------------------------------
-	
+
 }
